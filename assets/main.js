ea630edb4ddb7547a826b71f98bb1d2c6d591c29
kryderius <krydegamer@gmail.com>
2020-09-14 19:10:04 +0200

    commit

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,22 @@
 //ScrollTrigger Register Plugin
 gsap.registerPlugin(ScrollTrigger);
 
+/*
+$("html").easeScroll({
+  frameRate: 75,
+  animationTime: 1500,
+  stepSize: 80,
+  pulseAlgorithm: 1,
+  pulseScale: 6,
+  pulseNormalize: 1,
+  accelerationDelta: 20,
+  accelerationMax: 1,
+  keyboardSupport: true,
+  arrowScroll: 50,
+  touchpadSupport: true,
+  fixedBackground: true,
+});
+*/
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav-open");
 
@@ -10,32 +26,32 @@ const handleClick = () => {
 };
 hamburger.addEventListener("click", handleClick);
 
-gsap.from("#long-heading", 0.5, {
+gsap.from("#long-heading", 1, {
   x: "-100%",
-  ease: Power4.easeInOut,
+  ease: Power2.easeInOut,
 });
 gsap.from("#hero-h1", 1, {
   y: "-100%",
   opacity: 0,
-  ease: Power4.easeOut,
+  ease: Power4.easeInOut,
   delay: 0.5,
 });
 gsap.from("#hero-h1-stroke", 1, {
   y: "-100%",
   opacity: 0,
-  ease: Power4.easeOut,
+  ease: Power4.easeInOut,
   delay: 0.5,
 });
 gsap.from("#hero-h2", 1, {
   y: "-100%",
   opacity: 0,
-  ease: Power4.easeOut,
+  ease: Power4.easeInOut,
   delay: 1,
 });
 gsap.from("#hero-h2-stroke", 1, {
   y: "-100%",
   opacity: 0,
-  ease: Power4.easeOut,
+  ease: Power4.easeInOut,
   delay: 1,
 });
 
@@ -50,9 +66,56 @@ gsap.to("#hero-img", {
   yPercent: 20,
   ease: "none",
   scrollTrigger: {
-    trigger: "#hero-h1",
-    // start: "top bottom", // the default values
+    trigger: ".hero",
+    start: "top top", // the default values
     // end: "bottom top",
     scrub: true,
+  },
+});
+
+gsap.from("#designer", 1.5, {
+  x: "60%",
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".portfolio",
+    start: "top bottom", // the default values
+    end: "center bottom",
+    scrub: true,
+  },
+});
+gsap.from("#portfolio-h3", 1.5, {
+  y: "100%",
+  ease: Power4.easeInOut,
+  scrollTrigger: {
+    trigger: ".portfolio-header",
+    start: "top bottom", // the default values
+    // end: "bottom top",
+  },
+});
+gsap.to(".portfolio-img-span", 1, {
+  x: "100%",
+  ease: Power4.easeInOut,
+  scrollTrigger: {
+    trigger: ".portfolio-img",
+    start: "top 75%", // the default values
+    // end: "bottom top",
+  },
+});
+gsap.to(".cleantime-img-span", 1, {
+  x: "100%",
+  ease: Power4.easeInOut,
+  scrollTrigger: {
+    trigger: ".cleantime-img",
+    start: "top 75%", // the default values
+    // end: "bottom top",
+  },
+});
+gsap.to(".agachem-img-span", 1, {
+  x: "100%",
+  ease: Power4.easeInOut,
+  scrollTrigger: {
+    trigger: ".agachem-img",
+    start: "top 75%", // the default values
+    // end: "bottom top",
   },
 });
